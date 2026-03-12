55e39cf77a8bae983771c347b620a44bf1998cbe
Eric James <James3ric@gmail.com>
2026-03-12 22:13:32 +0000

    created functions for cleaning data

---
+++ src/clean.py
@@ -0,0 +1,42 @@
+import pandas as pd 
+
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean the input DataFrame by performing various data cleaning operations.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame to be cleaned.
+
+    Returns:
+        pd.DataFrame: The cleaned DataFrame.
+    """
+    # Remove duplicates
+    df = df.drop_duplicates()
+    
+    # renaming columns 
+
+    df = df.rename(columns={"prices_(£)": "price_gbp",
+                           "names": "product_name",
+                            "prices_unit_(£)": "price_per_kg",
+                            "supermarket": "retailer"})  # Rename columns for consistency
+    
+
+    # fixing date format
+    df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")  # Convert to datetime, coerce errors to NaT
+
+
+    # fixing own brand to boolean 
+    df["own_brand"] = df["own_brand"].str.strip().str.upper().map({"TRUE": True, "FALSE": False}).astype("boolean")  # Convert to boolean
+
+    # fixing retailers name
+    df["retailer"] = df["retailer"].str.replace("Sains", "Sainsbury's", regex=False)  # Standardize retailer names
+
+    # dropping nulls
+
+    df = df.dropna(subset=["price_gbp", "product_name", "price_per_kg", "own_brand"])  # Drop rows with any null values
+
+    # resetting index
+    df = df.reset_index(drop=True)  # Reset index after dropping rows
+
+    print (f"✅ Cleaned data: {len(df)} rows remaining after cleaning.")
+    return df
